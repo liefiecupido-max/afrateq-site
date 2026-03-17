541ae2c7114a0cb325ca92a9ca4246966873904f
William Cupido <william@Williams-Mac-mini.local>
2026-03-17 17:48:50 +0200

    Fix GitHub Pages base path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
+const repo = 'afrateq-site';
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
@@ -6,6 +9,8 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '',
 };
 
 module.exports = nextConfig;
